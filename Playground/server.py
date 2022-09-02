caf9f9a5a1fbb2c77ac63b49ed71ebf9c46b379c
JacobGreen-git <jacoblgreen96@gmail.com>
2022-09-02 13:24:08 -0500

    Playground

--- Playground/server.py
+++ Playground/server.py
@@ -1,20 +1,22 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')  
+    return render_template('index.html')
 
 @app.route('/play')
-def play():
-    return render_template('index.html')  
+def one():
+    return render_template('index.html', )
 
+@app.route('/play/<int:num>')
+def two(num):
+    return render_template('index.html', times = num)
 
-# @app.route('/play/<num>')
-# def wordRepeat(num, word):
-#     return word * int(num)
+@app.route('/play/<int:num>/<color>')
+def three(num , color):
+    return render_template('index.html', times = num, color = color)
 
-
-if __name__=="__main__":  
-    app.run(debug=True) 
+if __name__ == "__main__":
+    app.run(debug=True)
